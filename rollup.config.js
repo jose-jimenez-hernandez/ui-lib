ac9511f70bbb6b360854f4da1655c63dd7b87880
Jose Jimenez <jose.jimenezhernandez@settlenetwork.com>
2022-03-30 14:21:01 -0300

    add new version of tailwind and rollupplugins

--- rollup.config.js
+++ rollup.config.js
@@ -3,38 +3,46 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import dts from 'rollup-plugin-dts';
 
 const packageJson = require("./package.json");
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      dir: 'lib',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      dir: 'lib',
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      config: {
-        path: './postcss.config.js',
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'lib',
+        format: 'cjs',
+        sourcemap: true,
       },
-      extensions: ['.css'],
-      minimize: true,
-      inject: {
-        insertAt: 'top',
+      {
+        dir: 'lib',
+        format: 'esm',
+        sourcemap: true,
       },
-    }),
-  ],
-};
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true, tsconfig: './tsconfig.json' }),
+      postcss({
+        config: {
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top',
+        },
+      }),
+    ],
+  },
+  {
+    input: packageJson.main,
+    output: [{ file: packageJson.types, format: 'es' }],
+    plugins: [dts()],
+  },
+];
